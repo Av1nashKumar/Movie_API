e373d5288c92db1ab84115ebd33b133d4828d436
Av1nashKumar <Avinash.7355@gmail.com>
2018-10-29 01:35:45 +0530

    second commit

--- script.js
+++ script.js
@@ -1,42 +1,38 @@
-// let token = df12b502;
+
 let title;
 let year;
 let plot;
 let myUrl;
 let verifyCheckStatus;
+// $('#loader').hide();
+$(document).ready(()=>{
 
-$(document).ready( () => {
-$('#loader').hide();
+
 $('#submit').click(function(){
-  verify();
+   verify();
   if(verifyCheckStatus == true){
-    getAllData();
+    getAllDetails();
   }
 
+}); 
 });
 
- 
-});
+
 
 let verify = () => {
 
 title = $('#title').val();
-if(title == null || title == "") {
+if(title == null || title == "" || title == undefined) {
  
  // no title 
  alert('No Title');
-<<<<<<< HEAD
  verifyCheckStatus = false; 
-=======
- verifyCheckStatus = false;	
->>>>>>> 1506ba0531be4378abd31def4cab54f78a71795b
-
 } else {
     
        year = $('#year').val();
        plot = $('#plot').val();
 
-       if(year == null || year =="") { 
+       if(year == null || year == "" || year.length > 4 || year == undefined) { 
        myUrl = '&t=' + title + '&plot='+ plot + '&r=json';
         
        } else {
@@ -46,66 +42,73 @@ if(title == null || title == "") {
        }
        
        verifyCheckStatus = true;
+       myUrl = 'http://www.omdbapi.com/?apikey=df12b502' + myUrl;
 
      }
 
 }
 
-<<<<<<< HEAD
-let getAllData = () => {  
-$.ajax({
-    type : 'GET',
-    // dataType : 'json',
-    async : true,
-    url : 'https://www.omdbapi.com/?apikey=df12b502' + myUrl,
-    success : (response) => {
-      alert(response.status);
-      console.log(response);
-=======
-let getAllData = () => {	
-$.ajax({
-    type : 'GET',
-    dataType : 'json',
-    async : true,
-    url : 'https://yts.unblocked.gdn/api/v2/list_movies.json',
-    success : (response) => {
-	    alert(response.status);
-    	console.log(response);
->>>>>>> 1506ba0531be4378abd31def4cab54f78a71795b
 
-    },
 
-    error : (err) => {
+
+
+  /*Fetching input from searchbar ends*/
+
+ let getAllDetails = () => {
     
-    alert(err.Error);
+        // To get response
+        $.ajax({
+          type:"GET",
+          url:myUrl,
+          success: (response) => {
+             console.log(response);
+             
+             $('#gallery_card1').html(`
+              <div class="card card-3">
+              <img class="my-card-img img-fluid " src="${response.Poster}" alt="Card image cap" >
+              <div class="card-body">
+              <h5 class="card-title">${response.Title}</h5>
+              <p class="card-text">${response.Plot}</p>
+              <p class="card-text"><small class="text-muted">Rating ${response.imdbRating} </small></p>
+              </div>
+              </div>`)
 
+
+
+
+            
+          },
+
+          timeout:10000,
+            error: function (jqXHR, exception) {
+        var msg = '';
+        if (jqXHR.status === 0) {
+            msg = 'Not connect.\n Verify Network.';
+        } else if (jqXHR.status == 404) {
+            msg = 'Requested page not found. [404]';
+        } else if (jqXHR.status == 500) {
+            msg = 'Internal Server Error [500].';
+        } else if (exception === 'parsererror') {
+            msg = 'Requested JSON parse failed.';
+        } else if (exception === 'timeout') {
+            msg = 'Time out error.';
+        } else if (exception === 'abort') {
+            msg = 'Ajax request aborted.';
+        } else {
+            msg = 'Uncaught Error.\n' + jqXHR.responseText;
+        }
+        alert(msg);
     },
 
-    beforeSend: () => {
-<<<<<<< HEAD
-      
-=======
-    	
->>>>>>> 1506ba0531be4378abd31def4cab54f78a71795b
-    $('#loader').show();
-    },
-
-    complete : () => {
-    $('#loader').hide();
+          beforeSend: () => {
+            $('#loader').show();
     
-    }
-}); //end of AJAX CALL
-<<<<<<< HEAD
-  
-  
-  
-  
-  
-=======
-	
-	
-	
-	
-	
->>>>>>> 1506ba0531be4378abd31def4cab54f78a71795b
-}
+          },
+
+          complete : () => {
+           $('#loader').hide();
+            }
+
+    });
+
+  }
